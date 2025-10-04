cc8fce467c680707a734f878d7fae3fffb350685
MadhurIsHere <madhurrastogi20@gmail.com>
2025-10-05 02:43:47 +0530

    basic animation done

--- Package/Entity/Entity.java
+++ Package/Entity/Entity.java
@@ -1,7 +1,15 @@
 package Entity;
+
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+
 // this class will have all the variables of the player
 public class Entity {
     public int x,y;
     public int speed;
 
+    public BufferedImage up1,up2,down1,down2,right1,right2,left1,left2;
+    public String direction;
+    public int spriteNum=1;
+    public int spriteCounter=0;
 }
